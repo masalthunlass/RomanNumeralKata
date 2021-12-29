a1a863d29d6e936cb95887f390c6c040f4360568
masalthunlass <masalthunlass@gmail.com>
2021-12-29 16:59:59 +0100

    Numeral to Roman : refacto

--- src/RomanNumberConverterDecomposeNumberStrategy.ts
+++ src/RomanNumberConverterDecomposeNumberStrategy.ts
@@ -5,9 +5,8 @@ type ArabicNumber = number;
 export class RomanNumberConverterDecomposeNumberStrategy {
 
     convertToRomanNumber = (arabicNumber: ArabicNumber): RomanNumber => {
-        if (arabicNumber === 3) return 'III';
-        if (arabicNumber === 2) return 'II';
-        return 'I';
+
+        return 'I'.repeat(arabicNumber);
     };
 
 
