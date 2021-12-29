93d5c15b04b7eeaf84717f90f62c9267f24f1e2d
masalthunlass <masalthunlass@gmail.com>
2021-12-29 17:14:36 +0100

    Numeral to Roman : refacto

--- src/RomanNumberConverterDecomposeNumberStrategy.ts
+++ src/RomanNumberConverterDecomposeNumberStrategy.ts
@@ -4,13 +4,16 @@ type ArabicNumber = number;
 
 export class RomanNumberConverterDecomposeNumberStrategy {
 
+
+      readonly RomanCharacter = { 5 : 'V', 1 : 'I'}
+
     convertToRomanNumber = (arabicNumber: ArabicNumber): RomanNumber => {
         let romanNumber = '';
         if (arabicNumber >= 5) {
-            romanNumber += 'V';
+            romanNumber += this.RomanCharacter[5];
             arabicNumber = arabicNumber - 5;
         }
-         romanNumber +='I'.repeat(arabicNumber);
+         romanNumber += this.RomanCharacter[1].repeat(arabicNumber);
         return romanNumber;
     };
 
