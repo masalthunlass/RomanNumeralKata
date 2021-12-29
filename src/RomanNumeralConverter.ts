6ed2a0436f2f9c6f86ff40aff2fa7c0dae8999a2
masalthunlass <masalthunlass@gmail.com>
2021-12-29 13:31:33 +0100

    Numeral to Roman : refacto

--- src/RomanNumeralConverter.ts
+++ src/RomanNumeralConverter.ts
@@ -1,3 +1,7 @@
+enum RomanCharacter {
+    I=1, V = 5
+
+}
 
 type RomanNumber = string;
 type ArabicNumber = number;
@@ -8,13 +12,13 @@ export class RomanNumeralConverter {
 
     convertToRomanNumeral = (arabicNumber: ArabicNumber): RomanNumber => {
         let romanNumber = '';
-        if (arabicNumber == 4) return 'IV';
+        if (arabicNumber == 4) return  RomanCharacter[1]+RomanCharacter[5];
         if (arabicNumber >= 5) {
-            romanNumber = 'V';
+            romanNumber = RomanCharacter[5];
             arabicNumber= arabicNumber-5;
         }
         for (let i = 0; i < arabicNumber;i++) {
-            romanNumber += 'I';
+            romanNumber += RomanCharacter[1];
         }
         return romanNumber;
 
